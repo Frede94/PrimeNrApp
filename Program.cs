3bed80efa8bba6fe4d853fc135315cb1eb841f55
Frede94 <32297979+Frede94@users.noreply.github.com>
2022-03-21 20:23:36 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace PrimeNrApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string choice = "";
+            while (choice != "q")
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("-- a: run IsPrime method.      --");
+                Console.WriteLine("-- b: run CountPrimes method.  --");
+                Console.WriteLine("-- q: to quit program.         --");
+                Console.WriteLine("---------------------------------");
+
+                choice = Console.ReadLine();
+                if (choice == "a")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Write your Nr.");
+                    string input = Console.ReadLine();
+                    try
+                    {
+                        IsPrime(input);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Ikke et nr",e);
+                    }
+                }
+                else if (choice == "b")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Write start nr.");
+                    string startNr = Console.ReadLine();
+                    Console.WriteLine("Write ending nr.");
+                    string endNr = Console.ReadLine();
+
+                    try 
+                    { 
+                        CountPrimes(startNr, endNr); 
+                    }
+                    catch(Exception f)
+                    {
+                        Console.WriteLine("TEMP Wrong", f);
+                    }
+                }
+
+            }
+        }
+
+        /**
+         *  Check if the number is prime,
+         *  return true or false based on the result.
+         */
+        static bool IsPrime(string nr)
+        {
+            Int64 n = Convert.ToInt64(nr);
+
+            if (n == 2 || n == 3)
+            {
+                Console.WriteLine("true");
+                return true;
+            }
+
+            if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+            {
+                Console.WriteLine("false");
+                return false;
+            }
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                {
+                    Console.WriteLine("false");
+                    return false;
+                }
+            }
+
+            Console.WriteLine("true");
+            return true;
+        }
+        /**
+         *  Find the prime numbers between 2 numbers.
+         *  takes two string as search parameters.
+         */
+        public static string CountPrimes(string start, string end)
+        {
+            int sayac = 0;
+            string primes = "";
+            Int64 startNr = Convert.ToInt64(start);
+            Int64 endNr = Convert.ToInt64(end);
+            Int64 nrOfPrimes = 0;
+
+            Console.WriteLine("Prime numbers between {0} and {1} are: ", start, end);
+
+            for (Int64 i = startNr; i < endNr; i++)
+            {
+                sayac = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            sayac = 1;
+                            break;
+                        }
+                    }
+                    if (sayac == 0)
+                    {
+                        primes += i.ToString() + ", ";
+                        Console.WriteLine(i);
+                        nrOfPrimes++;
+                    }
+                }
+            }
+            Console.Clear();
+            Console.WriteLine(primes);
+            Console.WriteLine("");
+            Console.WriteLine("There is " + nrOfPrimes + " prime numbers inbetween " + start + " and " + end);
+
+            return primes;
+        }
+    }
+}
